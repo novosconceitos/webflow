597a0400d2356da18578bb3636cefdb7d1708976
novosconceitos <fabricio@novosconceitos.com.br>
2020-01-29 19:42:19 -0300

    Update filter-ncshowcase.js

--- filter-ncshowcase.js
+++ filter-ncshowcase.js
@@ -187,8 +187,8 @@ $(".slider").on("change", function() {
   
   var minValue = Number($("#minRange").val());
   var maxValue = Number($("#maxRange").val());
-  //document.getElementById("maxvalueShow").innerHTML = maxValue;
-  //document.getElementById("minvalueShow").innerHTML = minValue;
+  document.getElementById("maxvalueshow").innerHTML = "Número Máximo: " + maxValue;
+  document.getElementById("minvalueshow").innerHTML = "Número Mínimo: " + minValue;
 
   var numMin = Number($("#minvalueShow").text()); 
   var numMax = Number($("#maxvalueShow").text()); 
